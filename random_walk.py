68b869ec0abfa56f85ae6118f399335092ad93f7
abdelos <168035775+abdelos1@users.noreply.github.com>
2025-11-08 04:59:19 +0100

    Add files via upload

---
+++ random_walk.py
@@ -0,0 +1,37 @@
+#
+#Libraries
+import random #
+import matplotlib.pyplot as plt #
+import numpy as np #for linear regression
+from scipy.stats import linregress
+
+ndim=1200000
+npas=1000000
+nsim=100
+x=np.zeros(npas)
+y=np.zeros(npas)
+for i in range(nsim):
+    position=0
+    a=1
+    for ipas in range(npas):
+        r=random.random() 
+        if r<0.5: #equiprobable
+            istep=+a
+        else:
+            istep=-a
+        position+=istep
+        
+        x[ipas]+=position
+        y[ipas]+=position**2 
+        
+x=x/nsim
+y=y/nsim
+deltaxy=np.sqrt(y-x**2)
+# steps rank array
+step=np.arange(1,ndim)
+#a,b=np.polyfit(x,1)
+a,b,coef,pval,std=linregress(np.log(step),np.log(x))
+print(a,b)
+plt.plot(step,x)
+#plt.loglog(step,deltaxy,label='échelle logarithmique')
+plt.show()
