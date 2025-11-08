ecfe7a417d7156bf6dac2f50ec81a2e77d54774d
Abdel <orousanouabdelnasser@gmail.com>
2025-11-08 14:15:26 +0100

    rectifications

--- random_walk.py
+++ random_walk.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt #
 import numpy as np #for linear regression
 from scipy.stats import linregress
 
-ndim=1200000
+ndim=1000000
 npas=1000000
 nsim=100
 x=np.zeros(npas)
@@ -28,10 +28,9 @@ x=x/nsim
 y=y/nsim
 deltaxy=np.sqrt(y-x**2)
 # steps rank array
-step=np.arange(1,ndim)
+step=np.arange(1,ndim+1)
 #a,b=np.polyfit(x,1)
-a,b,coef,pval,std=linregress(np.log(step),np.log(x))
-print(a,b)
+#a,b,coef,pval,std=linregress(np.log(step),np.log(x))
 plt.plot(step,x)
 #plt.loglog(step,deltaxy,label='échelle logarithmique')
 plt.show()
